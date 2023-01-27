2c628131987d3ceda20bb6164632f46da4c0216e
Gilles GROS <gilles@71yeti.fr>
2023-01-27 17:41:20 +0100

    cleaning

--- webpack.prod.js
+++ webpack.prod.js
@@ -17,8 +17,21 @@ module.exports = merge(common(true), {
   },
   plugins: [
     new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: ["*", "!favicon.ico", "!CNAME", "!rules", "!rules.fr", "!img", "!ads.txt"],
-      cleanAfterEveryBuildPatterns: ["cards*.*", "*.ttf", "*.otf", "*.png"]
+      verbose: true,
+      protectWebpackAssets: false,
+      cleanOnceBeforeBuildPatterns: ["*", 
+          "!favicon.ico", 
+          "!CNAME", 
+          "!rules", 
+          "!rules.fr", 
+          "!img", 
+          "!ads.txt"],
+      cleanAfterEveryBuildPatterns: [
+          "*.map", 
+          "*.LICENSE.txt", 
+          "*.ttf", 
+          "*.otf", 
+          "*.png"]
     }),
   ]
 });
